a53013adcc9b078984ee2b37ef11121ac769cffb
Gabriel Franca <gabrielfrancasantana190504@gmail.com>
2023-12-07 18:28:16 -0500

    initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import { eachDayOfInterval, isSaturday, isSunday, format } from 'date-fns'
 import './App.css'
 
+let holidays = [
+  { date: '2023-01-01', name: 'New Year' },
+  { date: '2023-04-07', name: 'Good Friday' },
+  { date: '2023-04-10', name: 'Easter Monday' },
+  { date: '2023-05-22', name: 'Victoria Day' },
+  { date: '2023-06-24', name: 'Saint-Jean-Baptiste Day' },
+  { date: '2023-07-01', name: 'Canada Day' },
+  { date: '2023-08-07', name: 'Civic Holiday' },
+  { date: '2023-09-04', name: 'Labour Day' },
+  { date: '2023-09-30', name: 'National Day for Truth and Reconciliation' },
+  { date: '2023-10-09', name: 'Thanksgiving Day' },
+  { date: '2023-11-11', name: 'Remembrance Day' },
+  { date: '2023-12-25', name: 'Christmas Day' },
+  { date: '2023-12-26', name: 'Boxing Day' },
+]
+
 function App() {
-  const [count, setCount] = useState(0)
+  let year = 2023
+  let startDate = new Date(year, 0, 1)
+  let endDate = new Date(year, 11, 31)
+  let days = eachDayOfInterval({ start: startDate, end: endDate })
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>My calendar</h1>
+
+    <div className="calendar">
+    {days.map((day) => {
+        let isWeekend = isSaturday(day) || isSunday(day)
+        let feriado  = (holiday) => format(day, 'yyyy-MM-dd') === holiday.date
+        let isHoliday = holidays.some(feriado)
+        let dayClasses = `day ${isWeekend ? 'weekend' : ''} ${isHoliday ? 'holiday' : ''}`
+
+        return (
+          
+        )
+      })}
+    </div>
     </>
   )
 }
